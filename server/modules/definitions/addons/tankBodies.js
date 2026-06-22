5f05398d405496ff5453f46751049c9b527738f3
wplude114 <wplude@icloud.com>
2026-06-22 12:48:39 -0500

    Add tankBodies.js module for tank definitions

---
+++ server/modules/definitions/addons/tankBodies.js
@@ -0,0 +1,33 @@
+const { skillSet } = require('../facilitators.js');
+const { base, statnames, dfltskl, smshskl } = require('../constants.js');
+require('./generics.js');
+const g = require('../gunvals.js');
+
+console.log("[tankBodies.js] | ");
+return console.log("[tankBodies.js] | This addon is disabled by default! Please comment out or remove this line (line 6) to enable the addon.");
+
+// create tank
+Class.tankTreads = {
+    PARENT: "genericEntity",
+    TYPE: "tank",
+    COLOR: 17,
+    SHAPE: [[-1.5,-1.5],[1.5,-1.5],[1.5,1.5],[-1.5,1.5]], // rectangle but also kind of a square
+}
+Class.tankBody = {
+    PARENT: "genericEntity",
+    LABEL: "Body", // we dont need this, but if the body were to have a gun this would be useful
+    INDEPENDENT: true,
+    FACING_TYPE: "smoothWithMotion",
+    MOTION_TYPE: "motor",
+    COLOR: "mirror",
+    
+    SHAPE: [[-1.7,-1],[0,-1.3],[1.7,-1],[1.7,1],[0,1.3],[-1.7,1]], // long hexagon
+    CONTROLLERS: ["turretWithMotion", "scaleWithMaster"],
+    DECO: [{
+            POSITION: [20],
+            TYPE: "tankTreads",
+        }],
+}
+
+// we could just add the body to genericTank but that makes turrets have the body so we do this instead
+// note: make SPAWN_CLASS of config.js ["basic", "tankBody"]
